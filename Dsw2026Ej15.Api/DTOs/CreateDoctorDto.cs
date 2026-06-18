045a0a940a867b9ffba46a5f3ba20e3408c87ee1
Sergio V <sergio.vidaurri@alu.frt.utn.edu.ar>
2026-06-18 17:27:07 -0300

    Creacion de clases en API

---
+++ Dsw2026Ej15.Api/DTOs/CreateDoctorDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej15.Api.DTOs;
+
+public class CreateDoctorDto
+{
+    public string Name { get; set; } = string.Empty;
+    public string LicenseNumber { get; set; } = string.Empty;
+    public Guid SpecialityId { get; set; }
+}
